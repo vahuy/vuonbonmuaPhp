dc1c592034f7c8da1866630fef31d9ecf7f25e09
huy <anhuy121@gmail.com>
2019-01-07 16:01:19 +0700

    Create product views

---
+++ dashboard/Product.php
@@ -0,0 +1,53 @@
+<?php
+class Product
+{
+  public $id;
+  public $name;
+  public $image;
+  public $price;
+  public $shortDescription;
+  /*
+   * called by Dog, Cat, Bird, etc.
+   */
+  public function __construct($aId, $aName, $aImage, $aPrice, $aShortDescription)
+  {
+    $this->id = $aId;
+    $this->name = $aName;
+    $this->image = $aImage;
+    $this->price = $aPrice;
+    $this->shortDescription = $aShortDescription;
+  }
+
+  /*
+   * define the sorting rules - we will sort all Animals by name.
+   */ 
+  public static function getName() {
+    return $this->name;
+  }
+  
+  public static function getId() {
+    return $this->name;
+  }
+  
+  public static function getImage() {
+    return $this->image;
+  }
+
+  /*
+   * a String representation for all Animals.
+   */
+  public function __toString()
+  {
+    return "$this->name the $this->id goes $this->image";
+  }
+  public function generateHtml() {
+  	return "
+        <div class='col-sm-6 col-md-3'>
+            <img src=$this->image alt=$this->name>
+            <h1>$this->price</h1>
+            <p class='product-name'>$this->name</p>
+            <p>$this->shortDescription</p>
+        </div>";
+  }
+}
+?>
